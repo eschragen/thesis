b06cbf6d81ae2bc4e0173f7d1bf49349b8e0b86b
eschragen <58909233+eschragen@users.noreply.github.com>
2021-11-08 16:07:03 +0100

    .

--- 04_clean_tweets.R
+++ 04_clean_tweets.R
@@ -5,6 +5,7 @@ library(textclean)
 library(wordcloud)
 library(dplyr)
 library(ggplot2)
+library(textstem)
 
 setwd("C:/Users/eva_s/OneDrive/MASTER/5. Semester_THESIS/Data Analytics/DATA")
 df = read_csv("df")
@@ -13,25 +14,17 @@ tweets = df %>% select(id, tweet)
 #create subset of 1,000 tweets
 tweets_subset = tweets %>% sample_n(1000)
 
-#extract emojis, url, hashtags, mentions
-tweets_subset$tweet = gsub(">", "> ", tweets_subset$tweet)  # Add whitespace after every ">"
-tweets_subset$tweet = gsub("<", " <", tweets_subset$tweet)  # Add whitespace before every "<"
-tweets_subset$emojis = str_extract(tweets_subset$tweet, "<[^>]+>")
-tweets_subset$url = str_extract(tweets_subset$tweet, "http.+")
-tweets_subset$hashtag = str_extract(tweets_subset$tweet, "#\\S+")
-tweets_subset$mention = str_extract(tweets_subset$tweet, "@\\S+")
-
 #remove URL, mentions, emojis and hashtags
 tweets_subset$tweet = iconv(tweets_subset$tweet, to = "ASCII", sub = " ") #Remove non-ASCII characters
 tweets_subset$tweet = gsub("<[^>]+>", "", tweets_subset$tweet) #Remove emojis
 tweets_subset$tweet = gsub("http.+ |http.+$", "", tweets_subset$tweet) #Remove URLs
-tweets_subset$tweet = gsub("#\\S+", "", tweets_subset$tweet) #Remove hashtags
+tweets_subset$tweet = gsub("#", "", tweets_subset$tweet) #Remove hashtags
 tweets_subset$tweet = gsub("@\\S+", "", tweets_subset$tweet) #Remove Mentions
+tweets_subset$tweet = replace_internet_slang(tweets_subset$tweet) #Replace Slang Words
 tweets_subset$tweet = gsub("[[:digit:]]+", "", tweets_subset$tweet) #Remove numbers
 tweets_subset$tweet = gsub("[[:punct:]]+", "", tweets_subset$tweet) #Remove punctuation
 tweets_subset$tweet = gsub("[ |\t]{2,}", "", tweets_subset$tweet) #Remove tabs
 tweets_subset$tweet = str_trim(tweets_subset$tweet) #Remove unnecessary whitespace
-
 
 tweets_subset$tweet = tolower(tweets_subset$tweet) #Replace to lower words
 tweets_subset$tweet = replace_word_elongation(tweets_subset$tweet) #Replace word elongation
@@ -49,21 +42,26 @@ clean_corpus = function(corpus){
   # corpus = tm_map(corpus, removeNumbers)
   # corpus = tm_map(corpus, content_transformer(tolower))
   corpus = tm_map(corpus, removeWords,c(stopwords("en"),"vw", "volkswagen", "shell", "starbucks", 
-                                       "nestle", "mcdonalds", "mc donald's", "ikea", "hm", "exxonmobil", 
-                                       "coca cola", "unilever"))
-  corpus = tm_map(corpus, stemDocument, language = "english") #tbd
+                                       "nestle", "mcdonalds", "mc donald's", "ikea", "hm", "exxonmobil", "exxon mobil", 
+                                       "coca cola", "cocacola", "unilever", "amp"))
+  #corpus = tm_map(corpus, stemDocument, language = "english") #tbd
+  corpus = tm_map(corpus, lemmatize_strings)
+  corpus = tm_map(corpus, PlainTextDocument)
   return(corpus)
 }
 
 tweets_corpus_clean = clean_corpus(tweets_corpus)
 
 #get content of corpus
-# out = as.data.frame(sapply(tweets_corpus_clean, function(x){x$content}))
-# write.csv(out, file = "C:/Users/eva_s/OneDrive/MASTER/5. Semester_THESIS/Data Analytics/DATA/corpus.csv")
+content = as.data.frame(t((sapply(tweets_corpus_clean, function(x){x$content}))))
+content = content %>% drop_empty_row()
+# rownames(content) = c(1:nrow(content))
+# content = content[,1]
+# write.csv(content, file = "C:/Users/eva_s/OneDrive/MASTER/5. Semester_THESIS/Data Analytics/DATA/corpus.csv")
 
 #create wordcloud
 # set.seed(1234)
 # palet  = brewer.pal(8, 'Dark2')
-# wordcloud(tweets_corpus_clean, min.freq = 20, scale = c(4, 0.2) , random.order = TRUE, col = palet)
+# wordcloud(tweets_corpus_clean, min.freq = 10, scale = c(4, 0.2) , random.order = TRUE, col = palet)
 
 
--- 05_topic modeling.R
+++ 05_topic modeling.R
@@ -1,17 +1,58 @@
 #libraries
 library(tm)
 library(tidyverse)
+library(dplyr)
+library(topicmodels)
+library(tidytext)
+library(ggplot2)
 
-source()
+source("~/GitHub/thesis/04_clean_tweets.R")
 
 #prepare data for topic modeling: document-term matrix
 dtm = DocumentTermMatrix(tweets_corpus_clean)
 
-#show most frequent words
-freq = colSums(as.matrix(dtm))
-ord = order(freq, decreasing = TRUE)
-freq[head(ord, n = 20)]
+#Each row of the input matrix needs to contain at least one non-zero entry
+doc.length = apply(dtm, 1, sum)
+dtm = dtm[doc.length > 0,]
 
-plot = data.frame(words = names(freq), count = freq)
-plot = subset(plot, plot$count > 30) #creating a subset of words having more than 100 frequency
-ggplot(data = plot, aes(words, count)) + geom_bar(stat = 'identity') + ggtitle('Words used more than 150 times')+coord_flip()
+####DATA VISUALIZATION####
+##show most frequent words
+# freq = colSums(as.matrix(dtm))
+# ord = order(freq, decreasing = TRUE)
+# freq[head(ord, n = 20)]
+# 
+# plot = data.frame(words = names(freq), count = freq)
+# plot = subset(plot, plot$count > 50) #creating a subset of words having more than 100 frequency
+# ggplot(data = plot, aes(reorder(words,count), count)) + geom_bar(stat = 'identity') + ggtitle('Words used more than 50 times') +
+#   coord_flip() + labs(x = "words")
+
+##find associations mor most frequent words (e.g. "plastic", "water")
+# findAssocs(dtm, "plastic", 0.2)
+# findAssocs(dtm, "water", 0.2)
+#### ####
+
+#LDA topic modeling
+#try with k = 5
+lda = LDA(dtm, k = 5, method = 'Gibbs',
+          control = list(nstart = 5, seed = list(1505,99,36,56,88), 
+                         best = TRUE, thin = 500, burnin = 4000, 
+                         iter = 2000))
+
+#Top 10 terms or words under each topic
+top10terms = as.matrix(terms(lda,10))
+top10terms
+
+#Topics found out by the model
+lda.topics = as.matrix(topics(lda))
+summary(as.factor(lda.topics))
+
+#Get probabilities for each topic
+topicprob = as.matrix(lda@gamma)
+
+#Combine content of tweet with Topic Assignment
+tweets_topics = cbind(content, lda.topics)
+tweets_topics = tweets_topics %>% select(V1, lda.topics) 
+colnames(tweets_topics) = c("tweet", "topic")
+rownames(tweets_topics) = c(1:nrow(tweets_topics))
+
+ggplot(tweets_topics, aes(x = lda.topics)) + geom_bar()
